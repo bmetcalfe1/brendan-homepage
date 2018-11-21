987be8f8dbd576dc0d26c9d0934077a97a7999af
Brendan Metcalfe <brendanmetcalfe@Brendans-MacBook-Pro.local>
2018-11-21 15:50:39 -0500

    Updates

---
+++ precache-manifest.79f3bdfc9a5183f9a7284525f5599aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65e2dc9aad879cdba3d2",
+    "url": "/brendan-homepage/static/css/main.4a643c2c.chunk.css"
+  },
+  {
+    "revision": "65e2dc9aad879cdba3d2",
+    "url": "/brendan-homepage/static/js/main.65e2dc9a.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/brendan-homepage/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "ef33a983cdf6c343c30a",
+    "url": "/brendan-homepage/static/js/runtime~main.ef33a983.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/brendan-homepage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d079ba341ba515da0944445be6b89a9b",
+    "url": "/brendan-homepage/index.html"
+  }
+];
